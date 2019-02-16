78beffa6611e307d8b4c5f2a1d87d2d346a32b1c
Peter Savory <plsavory@outlook.com>
2019-02-16 00:24:28 +0000

    Remove unused code from the TestScreen

--- Game/Src/GameScreens/TestScreen.cpp
+++ Game/Src/GameScreens/TestScreen.cpp
@@ -15,26 +15,6 @@ TestScreen::TestScreen(Engine *enginePointer) {
   spriteRenderer = engine->getSpriteRenderer();
   resourceManager = engine->getResourceManager();
   textRenderer = engine->getTextRenderer();
-
-  // Load an image as a test
-  // resourceManager->loadTexture("resource\\uzume.jpg", "uzume");
-  // resourceManager->loadTexture("resource\\uzume2.jpg", "uzume2");
-  // resourceManager->loadTexture("resource\\natsuki.jpg", "natsuki");
-  // resourceManager->loadTexture("resource\\monika.png", "monika");
-  // spriteRenderer->addSprite("uzume", "uzume", 2);
-  // spriteRenderer->addSprite("uzume2", "uzume2", 1);
-  // spriteRenderer->addSprite("natsuki", "natsuki", 9);
-  // spriteRenderer->addSprite("monika", "monika", 0);
-
-  // Play some music as a test
-  // resourceManager->getMusicManager()
-  //   ->playAudioStream("main_theme");
-
-  // Draw some text as a test
-  // Text *text = textRenderer->addText("im_alive", "story_font");
-  // text->setString("I'm alive.");
-  // text->setPosition(550,400);
-
 }
 
 TestScreen::~TestScreen() {
